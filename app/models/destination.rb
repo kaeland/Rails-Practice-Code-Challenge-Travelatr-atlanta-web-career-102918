4644dea960252db5ec3303b8783d2798391c63e6
github username <github email address>
2018-11-29 18:13:34 -0500

    Our first commit

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,11 @@
 class Destination < ApplicationRecord
+  has_many :posts
+  has_many :bloggers, through: :posts 
 
+  def top_five
+    a = self.posts.sort_by do |post| 
+      post.updated_at
+    end
+    byebug
+  end
 end
